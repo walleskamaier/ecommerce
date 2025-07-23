a825e758e7ed56d8ee299d1b2a5ed5545d42df91
Walleska Maier <walleskamaier@gmail.com>
2025-07-23 13:05:00 -0300

    🚀 perf: implementa geração estática na build

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -24,11 +24,20 @@ async function getProduct(slug: string): Promise<Product> {
 export async function generateMetadata({
   params,
 }: ProductProps): Promise<Metadata> {
-  const product = await getProduct(params.slug) 
+  const product = await getProduct(params.slug)
 
   return {
     title: product.title,
   }
+}
+
+export async function generateStaticParams() {
+  const response = await api("/products/featured")
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
 }
 
 export default async function ProductPage({ params }: ProductProps) {
