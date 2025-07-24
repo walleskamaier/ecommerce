3e16fa361999a3be7504c3bbb200eca9c42ba2b9
Walleska Maier <walleskamaier@gmail.com>
2025-07-24 10:59:08 -0300

    ♻️refactor: separa carrinho do header e cria componentes client dedicados (cart-widget e add-to-cart-button)

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { api } from "@/data/api"
 import { Product } from "@/data/types/products"
 import { Metadata } from "next"
+import { AddToCartButton } from "@/components/add-to-cart-button"
 
 interface ProductProps {
   params: {
@@ -70,7 +71,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de{" "}
+            Em até 12x s/ juros de{" "}
             {(product.price / 12).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL",
@@ -107,12 +108,7 @@ export default async function ProductPage({ params }: ProductProps) {
             </button>
           </div>
         </div>
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
